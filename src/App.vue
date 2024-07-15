481fed0b4cf3ba58d9d15188a2af7a0c3675e313
YING-YING-LIN <change1869@gmail.com>
2024-07-15 13:35:02 +0800

    feat 新增bmi頁面

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/bmi">BMI</RouterLink>
       </nav>
     </div>
   </header>
