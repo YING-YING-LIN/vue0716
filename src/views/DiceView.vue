02eebbf62d97d059fbb69d61cfbf895ac31624eb
YING-YING-LIN <change1869@gmail.com>
2024-07-17 09:34:24 +0800

    feat 猜拳架構

--- src/views/DiceView.vue
+++ src/views/DiceView.vue
@@ -1,5 +1,26 @@
 <script></script>
 
-<template>1234</template>
+<template>
+    <div class="container">
+    <div><img src="./img/004.jpg" alt=""></div>
+    <button id="my_btn" type="button">開始擲骰子</button>
+    <div id="show_box"></div>
+    </div>
+    </template>
 
-<style></style>
+<style>
+.container{
+    margin: auto;
+    margin-top: 200px;
+}
+ img{
+            width: 100px;
+            height: 100px;
+            
+        }
+
+        #show_box{
+            font-family: Arial, sans-serif;
+            margin-top: 20px;
+        }
+</style>
